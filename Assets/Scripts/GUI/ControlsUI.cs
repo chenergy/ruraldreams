7b4834a39fb716e70e7b2363d3540d5dc4ad08a7
Jonathan Chien <jonathan.chien@gmail.com>
2013-10-09 08:23:19 -0700

    adjusted scene with sheep

--- Assets/Scripts/GUI/ControlsUI.cs
+++ Assets/Scripts/GUI/ControlsUI.cs
@@ -7,7 +7,7 @@ public class ControlsUI : MonoBehaviour {
 	
 	void OnGUI(){
 		GUI.Box(new Rect(20,0,200,100), 
-			"1 - Create Little Sheep\n2 - Create Big Sheep\nTab - Throw Sheep\nQ - Cycle Sheep Back\nE - Cycle Sheep To Front\nR - Open Door/Activate", 
+			"1 - Create Little Sheep\n2 - Create Big Sheep\nTab - Throw Sheep\nQ - Cycle Sheep Back\nE - Cycle Sheep To Front\nR - Open/Close Door, Pick-Up Sheep", 
 			leftJustified);
 		GUI.Box(new Rect(Screen.width - 220,0,200,100), 
 			"W - Forward\nA - Left\nD - Right\nS - Back\nSpace - Jump",
